e01b27d08019a751dd5ec92cd6a3698ff119de3a
Satrio Dian Nugroho <143977344+SATRIODIANNUGROHO@users.noreply.github.com>
2024-06-10 16:42:49 +0700

    Jobsheet Pertemuan 15 / Tugas 2

---
+++ Jobsheet 15/Tugas 2/Mahasiswa27.java
@@ -0,0 +1,43 @@
+package com.jobsheet15.tugas02;
+import java.util.*;
+class Mahasiswa27 {
+    String nimMhs;
+    String namaMhs;
+    String noTelpMhs;
+    public Mahasiswa27(String nimMhs, String namaMhs, String noTelpMhs) {
+        this.nimMhs = nimMhs;
+        this.namaMhs = namaMhs;
+        this.noTelpMhs = noTelpMhs;
+    }
+    public String getNim() {
+        return nimMhs;
+    }
+    public String getNama() {
+        return namaMhs;
+    }
+    public String getNoTelp() {
+        return noTelpMhs;
+    }
+    @Override
+    public String toString() {
+        return nimMhs + "\t" + namaMhs + "\t" + noTelpMhs;
+    }
+    public static void tampilDataMhs(List<Mahasiswa27> mahasiswa27s) {
+        int nimMhsWidth = 23;
+        int namaMhsWidth = 24;
+        int noTelpMhsmWidth = 24;
+        String header = "DAFTAR MAHASISWA";
+        System.out.printf("\n%45s\n", header);
+        System.out.println("+-----------------------+------------------------+------------------------+");
+        System.out.println("|          NIM          |          NAMA          |        NO. TELP        |");
+        System.out.println("+-----------------------+------------------------+------------------------+");
+        for (Mahasiswa27 mhs : mahasiswa27s) {
+            System.out.println("|" + padRight(mhs.nimMhs, nimMhsWidth) + "|" + padRight(mhs.namaMhs, namaMhsWidth) + "|" + padRight(mhs.noTelpMhs, noTelpMhsmWidth) + "|");
+        }
+        System.out.println("+-----------------------+------------------------+------------------------+");
+    }
+    // Method to pad string to the right
+    public static String padRight(String s, int n) {
+        return String.format("%-" + n + "s", s);
+    }
+}
---
+++ Jobsheet 15/Tugas 2/MataKuliah27.java
@@ -0,0 +1,43 @@
+package com.jobsheet15.tugas02;
+import java.util.*;
+class MataKuliah27 {
+    String kodeMatKul;
+    String namaMatKul;
+    int sksMatKul;
+    public MataKuliah27(String kodeMatKul, String namaMatKul, int sksMatKul) {
+        this.kodeMatKul = kodeMatKul;
+        this.namaMatKul = namaMatKul;
+        this.sksMatKul = sksMatKul;
+    }
+    public String getKodeMatKul() {
+        return kodeMatKul;
+    }
+    public String getNamaMatKul() {
+        return namaMatKul;
+    }
+    public int getSksMatKul() {
+        return sksMatKul;
+    }
+    @Override
+    public String toString() {
+        return kodeMatKul + "\t" + namaMatKul + "\t" + sksMatKul;
+    }
+    public static void daftarMatKul(List<MataKuliah27> mataKuliah27s) {
+        int kodeMatKulWidth = 23;
+        int namaMatKulWidth = 39;
+        int sksMatKulmWidth = 24;
+        String header = "DAFTAR MATA KULIAH";
+        System.out.printf("\n%54s\n", header);
+        System.out.println("+-----------------------+---------------------------------------+------------------------+");
+        System.out.println("|          KODE         |              MATA KULIAH              |           SKS          |");
+        System.out.println("+-----------------------+---------------------------------------+------------------------+");
+        for (MataKuliah27 matkuls : mataKuliah27s) {
+            System.out.println("|" + padRight(matkuls.kodeMatKul, kodeMatKulWidth) + "|" + padRight(matkuls.namaMatKul, namaMatKulWidth) + "|" + padRight(String.valueOf(matkuls.sksMatKul), sksMatKulmWidth) + "|");
+        }
+        System.out.println("+-----------------------+---------------------------------------+------------------------+");
+    }
+    // Method to pad string to the right
+    public static String padRight(String s, int n) {
+        return String.format("%-" + n + "s", s);
+    }
+}
